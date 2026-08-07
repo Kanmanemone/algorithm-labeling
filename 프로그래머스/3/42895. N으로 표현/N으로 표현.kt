e0f7c1f24a795f5a7650c611687dbbea7fdba70d
interfacer_han <30855675+Kanmanemone@users.noreply.github.com>
2026-08-07 13:27:40 +0900

    [level 3] Title: N으로 표현, Time: 5432.02 ms, Memory: 374 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
@@ -1,106 +1,76 @@
-fun main() {
-    println(Solution().solution(5, 12))
-    //println(Solution().solution(3, 11))
-}
-
-val minRecord = mutableMapOf<Int, Int>() // <node.number, node.depth> 저장
-val queue = SmartQueue()
+val queue = ArrayDeque<Node>()
+val countMemo = mutableMapOf<Int, Int>() // number, count
 
 class Solution {
     fun solution(N: Int, number: Int): Int {
-        queue.addIfNeeded(Node(N, 1))
-        while (!queue.isEmpty()) {
-            val node = queue.remove()
-            addNexts(node, N)
+        // 1
+        for (i in 1..8) {
+            queue.add(
+                Node(getRepeatedNumber(N, i), i)
+            )
+            countMemo[getRepeatedNumber(N, i)] = i
+        }
+        bfs { from ->
+            listOfNotNull(
+                Node(from.number + N, from.count + 1),
+                Node(from.number - N, from.count + 1),
+                Node(from.number * N, from.count + 1),
+                Node(from.number / N, from.count + 1)
+            )
         }
 
-        // log
-        minRecord.forEach { (key, value) ->
-            println("$key: $value")
+        // 2
+        queue.addAll(countMemo.map { Node(it.key, it.value) })
+        bfs { from ->
+            countMemo.flatMap { to ->
+                listOfNotNull(
+                    Node(from.number + to.key, from.count + to.value),
+                    Node(from.number - to.key, from.count + to.value),
+                    Node(from.number * to.key, from.count + to.value),
+                    if (to.key != 0) {
+                        Node(from.number / to.key, from.count + to.value)
+                    } else {
+                        null
+                    },
+                )
+            }
         }
 
-        minRecord.forEach { (number, depth) ->
-            queue.add(Node(number, depth))
-        }
-        while (!queue.isEmpty()) {
-            val node = queue.remove()
-            addNexts2(node)
-        }
+        return countMemo[number] ?: -1
+    }
+}
 
-        return minRecord[number] ?: -1
+fun bfs(getNextNodes: (Node) -> List<Node>) {
+    while (!queue.isEmpty()) {
+        val node = queue.removeFirst()
+        for (next in getNextNodes(node)) {
+            queue.addIfNeeded(next)
+        }
+    }
+}
+
+fun ArrayDeque<Node>.addIfNeeded(node: Node) {
+    if (8 < node.count) {
+        return
+    }
+
+    if (
+        (countMemo[node.number] == null)
+        || (node.count < countMemo[node.number]!!)
+    ) {
+        this.add(node)
+        countMemo[node.number] = node.count
     }
 }
 
 data class Node(
     val number: Int,
-    val depth: Int
+    val count: Int,
 )
 
-class SmartQueue {
-    private val queue = ArrayDeque<Node>()
-
-    fun addIfNeeded(node: Node) {
-        if (isAddNeeded(node)) {
-            queue.add(node)
-            minRecord[node.number] = node.depth
-        }
-    }
-
-    fun add(node: Node) {
-        queue.add(node)
-        minRecord[node.number] = node.depth
-    }
-
-    fun remove(): Node {
-        return queue.removeFirst()
-    }
-
-    fun isEmpty(): Boolean {
-        return queue.isEmpty()
-    }
-
-    private fun isAddNeeded(new: Node): Boolean {
-        return (minRecord[new.number] == null) // queue에 들어온 적 없거나
-                || (new.depth < (minRecord[new.number]!!)) // depth 갱신이 필요한 경우
-    }
-}
-
-fun addNexts(old: Node, N: Int) {
-    val depth = old.depth + 1
-    if (8 < depth) {
-        return
-    }
-    queue.addIfNeeded(Node(old.number + N, depth))
-    queue.addIfNeeded(Node(old.number - N, depth))
-    queue.addIfNeeded(Node(old.number * N, depth))
-    queue.addIfNeeded(Node(old.number / N, depth))
-    queue.addIfNeeded(Node(getRepeatedNumber(N, depth), depth))
-}
-
-fun addNexts2(old: Node) {
-    val oldNumber = old.number
-    val oldDepth = old.depth
-
-    minRecord.filter { new ->
-        val newDepth = new.value
-        old.depth + newDepth <= 8
-    }.forEach { new ->
-        val newNumber = new.key
-        val newDepth = new.value
-        val nextDepth = oldDepth + newDepth
-
-        queue.addIfNeeded(Node(oldNumber + newNumber, nextDepth))
-        queue.addIfNeeded(Node(oldNumber - newNumber, nextDepth))
-        queue.addIfNeeded(Node(oldNumber * newNumber, nextDepth))
-        if(newNumber != 0) {
-            queue.addIfNeeded(Node(oldNumber / newNumber, nextDepth))
-        }
-    }
-}
-
-fun getRepeatedNumber(N: Int, depth: Int): Int {
+fun getRepeatedNumber(N: Int, count: Int): Int {
     var result = 0
-    for (i: Int in 1..depth) {
+    for (i: Int in 1..count) {
         var operand = 1
         repeat(i - 1) {
             operand *= 10
